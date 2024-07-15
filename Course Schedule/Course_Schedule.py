34fa450ba3c93649a53eff2a47f927b43d04d320
Tiago Albuquerque de Lima <98188815+Tiago1604@users.noreply.github.com>
2024-07-15 19:45:57 -0300

    Course schedule versao 2

--- Course Schedule/Course_Schedule.py
+++ Course Schedule/Course_Schedule.py
@@ -3,42 +3,38 @@ class Solution:
         grafo1 = [[] for _ in range(n)]
         grafo2 = [[] for _ in range(n)]
         visitado = [False] * n
-        finalizar = [0] * n
-        tempo = [0]  
+        finalizado = [0] * n
+        tempo = [0]  # Use uma lista para passar por referência
 
-        for aresta in prerequisites:
-            de_no, para_no = aresta
+        for edge in prerequisites:
+            de_no, para_no = edge
             if de_no == para_no:
                 return False
             grafo1[de_no].append(para_no)
             grafo2[para_no].append(de_no)
 
-        ordens = list(range(n))
-        
         for i in range(n):
             if not visitado[i]:
-                self.dfs(grafo1, i, finalizar, tempo, visitado)
+                self.dfs(grafo1, i, visitado, finalizado, tempo)
 
-        ordens.sort(key=lambda x: finalizar[x], reverse=True)
+        ordens = list(range(n))
+        ordens.sort(key=lambda x: finalizado[x], reverse=True)
 
-        scc = 0
+        cfc = 0
         tempo[0] = 0
         visitado = [False] * n
-        
         for id in ordens:
             if not visitado[id]:
-                self.dfs(grafo2, id, finalizar, tempo, visitado)
-                scc += 1
+                self.dfs(grafo2, id, visitado, finalizado, tempo)
+                cfc += 1
 
-        return scc > 0
+        return cfc == n
 
-    def dfs(self, grafo, id, finalizar, tempo, visitado):
+    def dfs(self, grafo, id, visitado, finalizado, tempo):
         tempo[0] += 1
         visitado[id] = True
         for vizinho in grafo[id]:
             if not visitado[vizinho]:
-                self.dfs(grafo, finalizar, vizinho, tempo, visitado)
-                tempo[0] += 1
+                self.dfs(grafo, vizinho, visitado, finalizado, tempo)
         tempo[0] += 1
-        finalizar[id] = tempo[0]
-
+        finalizado[id] = tempo[0]
