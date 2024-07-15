22dab233467342cf5e7a34e98bf1949749362eb4
Tiago1604 <202028973@aluno.unb.br>
2024-07-15 15:40:29 -0300

    Soluções ex 2 e 3 versão 01

--- Terceiro exercicio/p3.py
+++ Terceiro exercicio/p3.py
@@ -1,1 +1,44 @@
+class Solution:
+    def canFinish(self, n, prerequisites):
+        grafo1 = [[] for _ in range(n)]
+        grafo2 = [[] for _ in range(n)]
+        visitado = [False] * n
+        finalizar = [0] * n
+        tempo = [0]  
+
+        for aresta in prerequisites:
+            de_no, para_no = aresta
+            if de_no == para_no:
+                return False
+            grafo1[de_no].append(para_no)
+            grafo2[para_no].append(de_no)
+
+        ordens = list(range(n))
+        
+        for i in range(n):
+            if not visitado[i]:
+                self.dfs(grafo1, i, finalizar, tempo, visitado)
+
+        ordens.sort(key=lambda x: finalizar[x], reverse=True)
+
+        scc = 0
+        tempo[0] = 0
+        visitado = [False] * n
+        
+        for id in ordens:
+            if not visitado[id]:
+                self.dfs(grafo2, id, finalizar, tempo, visitado)
+                scc += 1
+
+        return scc > 0
+
+    def dfs(self, grafo, id, finalizar, tempo, visitado):
+        tempo[0] += 1
+        visitado[id] = True
+        for vizinho in grafo[id]:
+            if not visitado[vizinho]:
+                self.dfs(grafo, finalizar, vizinho, tempo, visitado)
+                tempo[0] += 1
+        tempo[0] += 1
+        finalizar[id] = tempo[0]
 
--- segundo exercicio/p2.py
+++ segundo exercicio/p2.py
@@ -1,1 +1,32 @@
+class Solution:
+    def minimumCost(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        def encontrar(no: int) -> int:
+            if no == pai[no]: 
+                return no
+            pai[no] = encontrar(pai[no])
+            return pai[no]
+        
+        def unir(u: int, v: int, peso: int) -> int:
+            x, y = encontrar(u), encontrar(v)
+            peso_max[y] &= peso_max[x] & peso
+            pai[x] = y
+        
+        def encontrar_peso(consulta: list) -> int:
+            origem, destino = consulta
+            if origem == destino: 
+                return 0
+            if encontrar(origem) != encontrar(destino): 
+                return -1
+            return peso_max[encontrar(origem)]
+        
+        resposta = []
+        
+        for edge in edges:
+            pai = list(range(n))
+            peso_max = [(1<<31) - 1] * n
+            unir(*edge)
+        
+        return list(map(encontrar_peso, queries))
 
+pai = list(range(10))
+peso_max = [(1<<31) - 1] * 10
